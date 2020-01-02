3a066b40bf75217e6ad97c58a615327e2583269e
Chelsea Crooke <chelsea.crooke@yahoo.com>
2020-01-02 13:12:26 -0500

    heroku things added

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3000,
-    user: "root",
-    password: "Peachie44!!",
-    database: "burgers_db"
+    port: 3306,
+    user: "wf6o65bow2l2b1ub",
+    password: "g0euw4tueap42pae",
+    database: "qua8r8v0v2ap0um8"
 });
 
 connection.connect(function(err){

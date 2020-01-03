24d2d9a19025a4929a41650dca397bf862727213
Chelsea Crooke <chelsea.crooke@yahoo.com>
2020-01-02 20:19:44 -0500

    idk

--- public/js/script.js
+++ public/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
         $.ajax({
             method: "PUT",
-            url: "mysql://wf6o65bow2l2b1ub:g0euw4tueap42pae@ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/qua8r8v0v2ap0um8" + burger_id,
+            url: "?burgers/" + burger_id,
         }).then(function(_data){
             location.reload();
         });
